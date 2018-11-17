3868103584c8c07374fc21b02fd94eeb936edc2b
malenamuller <mmuller@itba.edu.ar>
2018-11-17 13:14:29 -0300

    arranco lo de pasar a vga

---
+++ ToVGA.v
@@ -0,0 +1,7 @@
+module ToVGA (clk,hsync,vsync,r,g,b);
+input wire clk;
+output wire hsync,vsync,r,g,b;
+parameter ADDRESS, FPORCH, BPORCH, HSYNC, VSYNC; //posicion de memoria donde arranca a estar guardada la pantalla que voy a mostrar.
+
+
+endmodule
