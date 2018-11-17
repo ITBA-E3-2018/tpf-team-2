ce0fd9ed0801977b1b6cb10837688a70ca9a9409
malenamuller <mmuller@itba.edu.ar>
2018-11-17 14:14:38 -0300

    tovga

--- ToVGA.v
+++ ToVGA.v
@@ -1,7 +1,9 @@
 module ToVGA (clk,hsync,vsync,r,g,b);
 input wire clk;
 output wire hsync,vsync,r,g,b;
-parameter ADDRESS, FPORCH, BPORCH, HSYNC, VSYNC; //posicion de memoria donde arranca a estar guardada la pantalla que voy a mostrar.
+parameter ADDRESS, FPORCH=5, BPORCH=5, HSYNC, VSYNC=40n; //posicion de memoria donde arranca a estar guardada la pantalla que voy a mostrar.
+//VER que poner en address, fporch, bporch
+
 
 
 endmodule
--- mod1.v
+++
@@ -1,8 +0,0 @@
-module mod1 (o, a, b);
-
-input a,b;
-output reg o;
-
-and (o, a, b);
-
-endmodule 
