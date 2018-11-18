280d857689fda0b5579dc9e1765e669eb24915f7
Ian Diaz <idiaz@itba.edu.ar>
2018-11-18 15:43:06 -0300

    Tested NUM27SEG, Works perfectly!

--- NumTo7Seg.v
+++ NumTo7Seg.v
@@ -1,6 +1,6 @@
-module NumTo7Seg (num,out);
-    input reg[3,0] num;
-    output reg [6,0] out;
+module NumTo7Seg (num,out);//RECIBE UN VALOR BINARIO DE 4 BITS Y LO TRANSFORMA A UN DISPLAY DE 7 SEGMENTOS
+    input [3:0] num;
+    output reg[6:0] out;
     parameter ZERO='b0000,ONE='b0001,TWO='b0010,THREE='b0011,FOUR='b0100,FIVE='b0101,SIX='b0110,SEVEN='b0111,EIGHT='b1000,NINE='b1001;
     always @(num) begin
         case (num)//7 segmentos = 'b (A B C D E F G)
