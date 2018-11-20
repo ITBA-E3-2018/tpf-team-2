854f94e69a3c7829e42feeba57d6c3a00c252d84
Benjamin Lin <blin@itba.edu.ar>
2018-11-20 15:18:11 -0300

    clk de 50 a 25M

--- contador/clk(internal).v
+++ contador/clk(internal).v
@@ -35,3 +35,22 @@ always @(posedge clkin) begin
         counter <= counter -1;
     end
 end
+
+module clock_50M_to_25M(clkin,clkout);
+
+reg counter;
+output reg clkout;
+input clkin
+initial begin
+    counter = 0;
+    clkout = 0;
+end
+
+always @(posedge clkin) begin
+    if (counter == 0) begin
+        counter <= 1;
+        clkout <= ~clkout;
+    end else begin
+        counter <= counter -1;
+    end
+end
