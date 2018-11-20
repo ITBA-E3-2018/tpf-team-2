d93bdae1a6f5a8dc4b80fd97406c906c1d41239c
Ian Diaz <idiaz@itba.edu.ar>
2018-11-20 16:14:17 -0300

    changed parameter names

--- Screen Positioning Module/PosPant.v
+++ Screen Positioning Module/PosPant.v
@@ -20,9 +20,9 @@ module PosPant(h_sinc,v_sinc,h1,h0,m1,m0,s1,s0,Rout,Gout,Bout,clk);//, countH,co
     reg [10:0] countH;
     parameter RETARDO_H = 48, RETARDO_V=33;
     parameter HOR_min=295, HOR_max=345, VER_min=(235+RETARDO_V), VER_max=(244+RETARDO_V);
-    parameter DH1m=(295+RETARDO_H),DH1M=(300+RETARDO_H), DH2m=(303+RETARDO_H), DH2M=(308+RETARDO_H),+
-        DH3m=(313+RETARDO_H), DH3M=(318+RETARDO_H), DH4m=(321+RETARDO_H), DH4M=(326+RETARDO_H), DH5m=(331+RETARDO_H),
-        DH5M=(336+RETARDO_H), DH6m=(339+RETARDO_H), DH6M = (344+RETARDO_H);
+    parameter DH1min=(295+RETARDO_H),DH1M=(300+RETARDO_H), DH2min=(303+RETARDO_H), DH2M=(308+RETARDO_H),+
+        DH3min=(313+RETARDO_H), DH3M=(318+RETARDO_H), DH4min=(321+RETARDO_H), DH4M=(326+RETARDO_H), DH5min=(331+RETARDO_H),
+        DH5M=(336+RETARDO_H), DH6min=(339+RETARDO_H), DH6M = (344+RETARDO_H);
 
 
     NumTo7Seg Disp(num,disp7);
@@ -62,29 +62,29 @@ module PosPant(h_sinc,v_sinc,h1,h0,m1,m0,s1,s0,Rout,Gout,Bout,clk);//, countH,co
         end
 
         if(inside == 1) begin
-            if(posX>=DH1m && posX <=DH1M) begin
+            if(posX>=DH1min && posX <=DH1M) begin
                 num = h1;
-                despl = DH1m;
+                despl = DH1min;
             end
-            if(posX>=DH2m && posX<=DH2M) begin
+            if(posX>=DH2min && posX<=DH2M) begin
                 num = h0;
-                despl = DH2m;
+                despl = DH2min;
             end
-            if(posX>=DH3m && posX<=DH3M) begin
+            if(posX>=DH3min && posX<=DH3M) begin
                 num = m1;
-                despl = DH3m;
+                despl = DH3min;
             end
-            if(posX>=DH4m && posX<=DH4M) begin
+            if(posX>=DH4min && posX<=DH4M) begin
                 num = m0;
-                despl = DH4m;
+                despl = DH4min;
             end
-            if(posX>=DH5m && posX<=DH5M) begin
+            if(posX>=DH5min && posX<=DH5M) begin
                 num = s1;
-                despl = DH5m;
+                despl = DH5min;
             end
-            if(posX>=DH6m && posX<=DH6M) begin
+            if(posX>=DH6min && posX<=DH6M) begin
                 num = s0;
-                despl = DH6m;
+                despl = DH6min;
             end
             posX1 = countH-despl;
         end
