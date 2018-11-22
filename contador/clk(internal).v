d10a4bc29ed81f73aad99dc21d67ca715c80b84f
Ian Diaz <idiaz@itba.edu.ar>
2018-11-21 21:07:29 -0300

    changed assignment

--- contador/clk(internal).v
+++ contador/clk(internal).v
@@ -1,23 +1,3 @@
-
-`timescale 20000ps / 2000ps
-
-module clock_gen_in(clk);
-
-parameter PERIOD = 2;
-
-output reg clk;
-	 
-always
-	begin: CLOCK_DRIVER
-		clk <= 1'b0;
-		#(PERIOD/2);
-		clk <= 1'b1;
-		#(PERIOD/2);
-	end
-endmodule
-
-
-
 module clock_50M_to_1k(clkin,clkout);
 
 reg [14:0] counter;
